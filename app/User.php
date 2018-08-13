28df0183411370e8bdad2cdf8feb2a02d53ea3ae
EC2 Default User <ec2-user@ip-172-31-3-175.us-east-2.compute.internal>
2018-08-13 07:40:16 +0000

    kadai-monolist

--- app/User.php
+++ app/User.php
@@ -35,6 +35,10 @@ class User extends Authenticatable
         return $this->items()->where('type','want');
     }
     
+    public function have_items() {
+        return $this->items()->where('type','have');
+    }
+    
     public function want($itemID){
         
         // 既に Want しているかの確認
@@ -64,6 +68,38 @@ class User extends Authenticatable
         }
     }
     
+    
+    //Have追加
+    public function have($itemID){
+        
+        // 既に Have しているかの確認
+        $exist = $this->is_having($itemID);
+        
+        if($exist){
+            // 既に have していれば何もしない
+            return false;
+        }else{
+            // 未 Have であれば Have する
+            $this->items()->attach($itemID,['type' => 'have']);
+            return true;
+        }
+    }
+    
+    public function dont_have($itemID){
+        
+        // 既に Have しているかの確認
+        $exist = $this->is_having($itemID);
+        
+        if($exist){
+            // 既に Have していれば Have を外す
+            \DB::delete("DELETE FROM item_user WHERE user_id = ? AND item_id = ? AND type = 'have'",[$this->id,$itemID]);
+        }else{
+            // 未 Have であれば何もしない
+            return false;
+        }
+    }
+    
+    
     public function is_wanting($itemIdOrCode){
         if(is_numeric($itemIdOrCode)){
             $item_id_exists = $this->want_items()->where('item_id',$itemIdOrCode)->exists();
@@ -74,5 +110,15 @@ class User extends Authenticatable
         }
     }
     
+    //Have追加
+    public function is_having($itemIdOrCode){
+        if(is_numeric($itemIdOrCode)){
+            $item_id_exists = $this->have_items()->where('item_id',$itemIdOrCode)->exists();
+            return $item_id_exists;
+        }else{
+            $item_code_exists = $this->have_items()->where('code',$itemIdOrCode)->exists();
+            return $item_code_exists;
+        }
+    }
     
 }
