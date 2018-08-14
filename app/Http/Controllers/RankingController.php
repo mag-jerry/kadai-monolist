877697a37bc35dc8eda0a788b3bf538471cd6c8d
EC2 Default User <ec2-user@ip-172-31-3-175.us-east-2.compute.internal>
2018-08-14 22:16:41 +0000

    ranking Have

--- app/Http/Controllers/RankingController.php
+++ app/Http/Controllers/RankingController.php
@@ -19,6 +19,14 @@ class RankingController extends Controller
         ]);
     }
    
+    public function have()
+    {
+        $items = \DB::table('item_user')->join('items', 'item_user.item_id', '=', 'items.id')->select('items.*', \DB::raw('COUNT(*) as count'))->where('type', 'have')->groupBy('items.id', 'items.code', 'items.name', 'items.url', 'items.image_url','items.created_at', 'items.updated_at')->orderBy('count', 'DESC')->take(10)->get();
+
+        return view('ranking.have', [
+            'items' => $items,
+        ]);
+    }
    
     /**
      * Display a listing of the resource.
