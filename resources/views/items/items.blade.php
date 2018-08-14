8109f56224faf3f1225e832046e9a78add4a50cb
EC2 Default User <ec2-user@ip-172-31-3-175.us-east-2.compute.internal>
2018-08-14 23:07:45 +0000

    Ranking syusei

--- resources/views/items/items.blade.php
+++ resources/views/items/items.blade.php
@@ -22,7 +22,7 @@
                         </div>
                         @if (isset($item->count))
                             <div class="panel-footer">
-                                <p class="text-center">{{ $key+1 }}位: {{ $item->count}} Haves</p>
+                                <p class="text-center">{{ $key+1 }}位: {{ $item->count}}</p>
                             </div>
                         @endif
                     </div>
