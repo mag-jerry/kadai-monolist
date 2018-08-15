52542bc2509d56ba60eba3125b2125121be018ae
EC2 Default User <ec2-user@ip-172-31-3-175.us-east-2.compute.internal>
2018-08-15 16:04:43 +0000

    kadai-ranking-syusei

--- app/Http/Controllers/RankingController.php
+++ app/Http/Controllers/RankingController.php
@@ -13,18 +13,20 @@ class RankingController extends Controller
     public function want()
     {
         $items = \DB::table('item_user')->join('items', 'item_user.item_id', '=', 'items.id')->select('items.*', \DB::raw('COUNT(*) as count'))->where('type', 'want')->groupBy('items.id', 'items.code', 'items.name', 'items.url', 'items.image_url','items.created_at', 'items.updated_at')->orderBy('count', 'DESC')->take(10)->get();
-
+        $coname = "Wants";
         return view('ranking.want', [
             'items' => $items,
+            'coname' => $coname,
         ]);
     }
    
     public function have()
     {
         $items = \DB::table('item_user')->join('items', 'item_user.item_id', '=', 'items.id')->select('items.*', \DB::raw('COUNT(*) as count'))->where('type', 'have')->groupBy('items.id', 'items.code', 'items.name', 'items.url', 'items.image_url','items.created_at', 'items.updated_at')->orderBy('count', 'DESC')->take(10)->get();
-
+        $coname = "Haves";
         return view('ranking.have', [
             'items' => $items,
+            'coname' => $coname,
         ]);
     }
    
--- resources/views/items/items.blade.php
+++ resources/views/items/items.blade.php
@@ -22,7 +22,7 @@
                         </div>
                         @if (isset($item->count))
                             <div class="panel-footer">
-                                <p class="text-center">{{ $key+1 }}位: {{ $item->count}}</p>
+                                <p class="text-center">{{ $key+1 }}位: {{ $item->count }} {{ $coname }}</p>
                             </div>
                         @endif
                     </div>
